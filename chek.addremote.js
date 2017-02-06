cf1a5c6ba2c1d38a5f44050905c20708c72d36cf
Robert Craig <robert.craig@gmail.com>
2017-02-05 18:36:28 -0600

    Update chek.addremote.js

--- chek.addremote.js
+++ chek.addremote.js
@@ -4,7 +4,7 @@ tools.CheckOrBuildDatabase();
 var upbody = "";
 
 var request = require('request');
-request('http://stuxred:3000/up.json', function (error, response, body) {
+request('http://stuxwood:3000/up.json', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         upbody = body
 	console.log(upbody);
